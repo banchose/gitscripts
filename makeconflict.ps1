f21d0a081f88522f5de71fc29ddbc7797562c4af
banchose <a_gitmail@ownmail.net>
2017-11-29 14:32:44 -0500

    Create makeconflict.ps1
    Create a repo set up to create a merge conflict to test git mergetool

---
+++ makeconflict.ps1
@@ -0,0 +1,25 @@
+$STARTDIR="X:\gitdir"
+
+mkdir $STARTDIR\zoo
+cd $STARTDIR\zoo
+
+git init
+
+echo "cat" >> animals.txt
+echo "dog" >> animals.txt
+echo "octopus" >> animals.txt
+echo "octocat" >> animals.txt
+
+git add animals.txt
+git commit -m "Initial commit"
+git branch octodog
+git checkout octodog
+$path="$STARTDIR\zoo\animals.txt"
+(Get-Content $path) -replace "octopus","octodog" | out-file $path
+git add animals.txt
+git commit -m "Replace octopus to octodog"
+git checkout master
+(Get-Content $path) -replace "octopus","octoman" | out-file $path
+
+git add animals.txt
+git commit -m "Replace octopus to octoman"
